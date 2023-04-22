ee5246265d7e61c8f935e94f370cf04e22389578
bird <mbird3@gmail.com>
2023-04-21 17:10:12 -0700

    create and test session controller and associated view

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,38 @@
+class Api::SessionsController < ApplicationController
+
+  # before_action :require_logged_in, only: [:show]
+
+  def show
+    if logged_in?
+      @user = current_user
+      render :show
+    else
+      render json: {user: nil}
+    end
+  end
+
+  def create
+    # debugger
+    email = params[:email]
+    password = params[:password]
+
+    @user = User.find_by_credentials(email, password)
+
+    if(@user)
+      login!(@user)
+      render :show
+    else
+      print "this is the else"
+      render json: { errors: ['The provided credentials were invalid.'] }, status: :unauthorized
+    end
+  end
+
+  def destroy
+    if logged_in?
+      logout!
+      render json: {message: 'success'}
+    else
+      render json: {message: 'no user was logged in'}
+    end
+  end
+end
