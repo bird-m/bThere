0ec488f60d12740c6898824508d91386d3ca7acb
bird <mbird3@gmail.com>
2023-04-27 12:12:27 -0700

    remove old logger import

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 // import reportsReducer from './reports';
-import logger from 'redux-logger'
+// import logger from 'redux-logger'
 import sessionReducer from './session';
 import formReducer from './formReducer';
 
@@ -17,7 +17,7 @@ let enhancer;
 if (process.env.NODE_ENV === 'production') {
     enhancer = applyMiddleware(thunk);
 } else {
-    // const logger = require('redux-logger').default;
+    const logger = require('redux-logger').default;
     const composeEnhancers =
         window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ 
         || compose;
