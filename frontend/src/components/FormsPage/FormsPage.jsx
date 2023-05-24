188e47d436069b070e8862229ec835c25754803f
bird <mbird3@gmail.com>
2023-05-23 23:11:25 -0700

    clean up question modification code and add validation

--- frontend/src/components/FormsPage/FormsPage.jsx
+++ frontend/src/components/FormsPage/FormsPage.jsx
@@ -9,14 +9,14 @@ import { Link } from 'react-router-dom';
 import checkLogo from '../../images/check-logo.png'
 import { LoggedInBanner } from "../LoggedInBanner/LoggedInBanner";
 
-export default function FormsPage (props) {
+export default function FormsPage(props) {
 
     const dispatch = useDispatch();
     const forms = useSelector(selectAllForms);
 
     useEffect(() => {
         dispatch(fetchForms())
-    },[dispatch])
+    }, [dispatch])
 
     // debugger;
 
@@ -29,16 +29,26 @@ export default function FormsPage (props) {
     return (
         <div className="form-page-wrapper">
             <div className="added-banner-wrap">
-                <LoggedInBanner/>
+                <LoggedInBanner />
             </div>
             <div className="form-nav">
-                <Link to="/form" className="form-button"><button>Create New Form</button></Link>
+                <div className="form-nav-left">
+                    <div className="form-tab form-nav-selected">
+                        Forms
+                    </div>
+                    <div className="contacts-tab">
+                        Contacts
+                    </div>
+                </div>
+                <div className="form-nav-right">
+                    <Link to="/form" className="form-button"><button>Create New Form</button></Link>
+                </div>
             </div>
             <div className="form-grid">
                 {forms.map((form) => {
                     return (
                         <div key={form.id} className="form-page-item">
-                            <FormSummary form={form}/>
+                            <FormSummary form={form} />
                         </div>
                     )
                 })}
@@ -50,9 +60,9 @@ export default function FormsPage (props) {
     // return (
     //     <>
     //         <div className="form-page-wrapper">
-                // <div className="form-page-item">
-                //     <FormSummary forms={forms}/>
-                // </div>
+    // <div className="form-page-item">
+    //     <FormSummary forms={forms}/>
+    // </div>
     //         </div>
     //     </>
     // )
--- frontend/src/components/QuestionPane/QuestionPane.jsx
+++ frontend/src/components/QuestionPane/QuestionPane.jsx
@@ -1,101 +1,73 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteQuestion, fetchQuestion, postQuestion, selectQuestion } from "../../store/questionReducer";
 import './QuestionPane.css'
-import {AiFillEdit} from 'react-icons/ai'
-import {FaCheck} from 'react-icons/fa'
-import {AiFillDelete} from 'react-icons/ai'
+import { AiFillEdit } from 'react-icons/ai'
+import { FaCheck } from 'react-icons/fa'
+import { AiFillDelete } from 'react-icons/ai'
+import ghLogo from "../../images/github-mark-white.png"
 
-export default function QuestionPane ({question, createMode, formId}) {
+export default function QuestionPane({ question, formId }) {
     // console.log(formId, "FORM ID IN QP")
-
-    if(createMode) {
-        question = {
-            prompt: "",
-            description: ""
-        }
-    }
 
     const [editMode, setEditMode] = useState(false);
     const [prompt, setPrompt] = useState(question.prompt);
     const [description, setDescription] = useState(question.description);
 
-    
+    const formRef = useRef(null);
     const dispatch = useDispatch();
 
-    function updateQuestion () {
-        let updatedQuestion = {question: {
-            prompt,
-            description
-        }}
-        // debugger
-
-        if (createMode) {
-            updatedQuestion.question.formId = formId
-            // debugger;
-            dispatch(postQuestion(updatedQuestion));
-            setDescription("");
-            setPrompt("");
-        } else {
-            dispatch(postQuestion(updatedQuestion, question.id))
-            setEditMode(false);
+    function updateQuestion(e) {
+        e.preventDefault()
+        let updatedQuestion = {
+            question: {
+                prompt,
+                description
+            }
         }
+
+        dispatch(postQuestion(updatedQuestion, question.id))
+        setEditMode(false);
     }
 
-    function actionButton() {
-        if (editMode) {
-            return <button onClick={updateQuestion}>Save</button>
-        } else if (createMode) {
-            return null
-        }
-        else {
-            return (<>
-                <button onClick={() => setEditMode(true)}>Edit</button>
-                <button onClick={() => {dispatch(deleteQuestion(question.id))}}>Delete</button>
-            </>)
-        }
-    }
+    // 008001
 
     function injectIcons() {
         if (editMode) {
             return (
-            <>
-            <div></div>
-            <div className="qp-mod-icon qp-save-icon" onClick={updateQuestion}><FaCheck/></div>
-            </>)
-        } else if (createMode) {
-            return null;
+                <>
+                    <div></div>
+                    <button id="qp-save-check" className="qp-mod-icon qp-save-icon"><FaCheck /></button>
+                </>)
         }
         else {
             return (<>
-                    <div className="qp-mod-icon qp-del-icon" onClick={() => {dispatch(deleteQuestion(question.id))}}><AiFillDelete/></div>
-                    <div className="qp-mod-icon qp-edit-icon" onClick={() => setEditMode(true)}><AiFillEdit/></div>
-                </>)
+                <div className="qp-mod-icon qp-del-icon" onClick={() => { dispatch(deleteQuestion(question.id)) }}><AiFillDelete /></div>
+                <div className="qp-mod-icon qp-edit-icon" onClick={() => setEditMode(true)}><AiFillEdit /></div>
+            </>)
         }
     }
 
     // let a = {question: {formId: 1, prompt: "updated another",required: true, description: "hd"}}
-    
+
     // return null;
-    return (<div className="qp-wrapper">
-        <div className="qp-mod-button-wrapper">
-            {injectIcons()}
-        </div>
-        <div className="qp">
-            <div className="qp-ele">
-                {(editMode || createMode) ? <input placeholder="enter the question here" type="text" value={prompt} onChange={(e) => {setPrompt(e.target.value)}}/> : <h2>{prompt}</h2>}
-                
+    return (
+        <form id="myForm" className="qp-wrapper" ref={formRef} onSubmit={(e) => { updateQuestion(e) }}>
+
+            <div className="qp-mod-button-wrapper">
+                {injectIcons()}
             </div>
-
-            {!description && !(editMode || createMode) ? null :
+            <div className="qp">
                 <div className="qp-ele">
-                    {(editMode || createMode) ? <textarea placeholder="enter a description here" value={description} onChange={(e) => {setDescription(e.target.value)}}/> : <h2>{description}</h2>}
+                    {(editMode) ? <input placeholder="enter the question here" required type="text" value={prompt} onChange={(e) => { setPrompt(e.target.value) }} /> : <h2>{prompt}</h2>}
+
                 </div>
-            }
 
-
-            {/* {actionButton()} */}
-
-        </div>
-    </div>);
+                {!description && !(editMode) ? null :
+                    <div className="qp-ele">
+                        {(editMode) ? <textarea placeholder="enter a description here" value={description} onChange={(e) => { setDescription(e.target.value) }} /> : <h2>{description}</h2>}
+                    </div>
+                }
+            </div>
+        </form>);
 }
