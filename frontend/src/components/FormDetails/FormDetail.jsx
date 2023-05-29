6f0fa0a30d13379b8cf923b052694b4312294db9
bird <mbird3@gmail.com>
2023-05-29 15:00:29 -0500

    add share modal

--- frontend/src/components/FormDetails/FormDetail.jsx
+++ frontend/src/components/FormDetails/FormDetail.jsx
@@ -8,6 +8,7 @@ import {TbShare2} from 'react-icons/tb'
 import { useState } from 'react';
 import Modal from '../Modal/Modal'
 import DeletFormConfirmation from '../DeleteFormConfirmation/DeleteFormConfirmation';
+import ShareSheet from '../ShareSheet/ShareSheet';
 
 // MdModeEdit
 
@@ -16,20 +17,27 @@ export default function FormDetail ({form}) {
     const history = useHistory();
     const dispatch = useDispatch();
     const [showDeleteModal, setShowDeleteModal] = useState(false);
+    const [showShareModal, setShowShareModal] = useState(false);
 
     function closeModals() {
         showDeleteModal && setShowDeleteModal(false);
+        showShareModal && setShowShareModal(false);
     }
 
     function openDeleteModal() {
         setShowDeleteModal(true);
     }
+
+    function openShareModal() {
+        setShowShareModal(true);
+    }
     
     return (
         <div className="detail-wrapper">
             {showDeleteModal && <Modal closeModal={closeModals} content={<DeletFormConfirmation formId={form.id} closeModal={closeModals}/>}/>}
+            {showShareModal && <Modal closeModal={closeModals} content={<ShareSheet form={form} closeModal={closeModals}/>}/>}
             <div className="detail-share-icon">
-                <Link to={`/submit/${form.id}`}><TbShare2/></Link>
+                <Link to="#" onClick={openShareModal}><TbShare2/></Link>
             </div>
             <div className="detail-title">
                 <Link to={`/form/configure/${form.id}`}>{form ? form.title : "loading..."}</Link>
---
+++ frontend/src/components/ShareSheet/ShareSheet.jsx
@@ -0,0 +1,43 @@
+import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
+import './ShareSheet.css'
+import { MdOutlineCancel } from 'react-icons/md'
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useRef } from 'react';
+
+export default function ShareSheet({ form, closeModal }) {
+
+    const [showCopyMessage, setShowCopyMessage] = useState(false);
+    const [timer, setTimer] = useState();
+
+    async function handleCopyClick() {
+        if (!showCopyMessage) {
+            await navigator.clipboard.writeText(window.location.origin + "/" + (form.customUrl ? form.customUrl : "hello"));
+            setShowCopyMessage((prev) => (!prev));
+        }
+    }
+
+    function handleClose() {
+        closeModal();
+    }
+
+    return <div className="dlc-wrapper">
+        <div className="closer-header">
+            <span className='delete-cancel-icon' onClick={handleClose} ><MdOutlineCancel /></span>
+        </div>
+        <div className="dlc-text">
+            Share this link with friends to have them fill out your form!
+        </div>
+        <div className="share-field">
+            <div className="ss-share-link-wrapper">
+                <div className="ss-share-link">
+                    {showCopyMessage && <div className="copied-message">
+                        Copied to clipboard!
+                    </div>}
+                    {window.location.origin + "/" + (form.customUrl ? form.customUrl : "hello")}
+                </div>
+                <div className="share-field-copy" onClick={handleCopyClick}>Copy</div>
+            </div>
+        </div>
+    </div>;
+}
