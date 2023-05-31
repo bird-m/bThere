a3511f74c315646633bb3c2ce0d3feb696db1737
bird <mbird3@gmail.com>
2023-05-31 16:19:36 -0700

    create add contact modal

--- frontend/src/components/ContactsPage/ContactsPage.jsx
+++ frontend/src/components/ContactsPage/ContactsPage.jsx
@@ -1,19 +1,23 @@
 import { useEffect, useState } from 'react'
 import './ContactsPage.css'
-import {AiOutlinePlusCircle} from 'react-icons/ai'
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteContact, fetchContacts, postContact, selectContacts } from '../../store/contactReducer';
 import ContactPane from '../CreationPane/ContactPane';
 import ContactModifier from '../ContactModifier/ContactModifier';
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import ContactEntry from '../ContactEntry/ContactEntry';
+import Modal from '../Modal/Modal';
 
 export default function ContactsPage () {
 
     const {formId} = useParams();
 
     const dispatch = useDispatch();
+    const [showContactModal, setShowContactModal] = useState(true);
 
-    const [email, setEmail] = useState("");
+    function closeContactModal() {
+        setShowContactModal(false);
+    }
 
     useEffect(() => {
         dispatch(fetchContacts(formId))
@@ -21,25 +25,10 @@ export default function ContactsPage () {
 
     const contacts = useSelector(selectContacts);
 
-    function handleContactCreate (e) {
-        e.preventDefault();
-        const newContact = {contact: {
-            email
-        }}
-
-        dispatch(postContact(newContact));
-        setEmail("");
-    }
-
-    // console.log()
+    
     return (
-        <div className="contact-show">
-            <form className="contact-entry" onSubmit={(e) => {handleContactCreate(e)}}>
-                <label htmlFor='contact-email'>Add new contact</label>
-                <input placeholder='contact email' type="email" required htmlFor='contact-email' value={email} onChange={(e) => {setEmail(e.target.value)}}/>
-                <button className='svg-button dark-grey'><AiOutlinePlusCircle/></button>
-                
-            </form>
+        <div className="contact-show">            
+            {showContactModal && <Modal closeModal={closeContactModal} content={<ContactEntry closeModal={closeContactModal}/>}/>}
             <div className="contacts-header">
                 {contacts ? "CONTACTS" : ""}
             </div>
