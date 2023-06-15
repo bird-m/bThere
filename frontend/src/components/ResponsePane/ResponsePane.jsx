52cbce279266e04e49a11c45b8260eb45cc4276e
bird <mbird3@gmail.com>
2023-06-15 13:35:05 -0400

    optimize response flow

--- frontend/src/components/ResponsePane/ResponsePane.jsx
+++ frontend/src/components/ResponsePane/ResponsePane.jsx
@@ -1,13 +1,16 @@
 import { useEffect, useRef, useState } from 'react';
 import './ResponsePane.css'
 
-export default function ResponsePane ({question, setRefs, refs}) {
+export default function ResponsePane({ question, setRefs, refs }) {
 
     const [response, setResponse] = useState("");
     const inputRef = useRef();
 
-    useEffect(()=> {
-        setRefs((prevRefs) => ({...prevRefs, [question.id]: inputRef}));
+    useEffect(() => {
+        setRefs((prevRefs) => {
+            prevRefs[question.id] = inputRef
+            return prevRefs;
+        });
     }, [])
 
     return (
@@ -15,7 +18,7 @@ export default function ResponsePane ({question, setRefs, refs}) {
             <label htmlFor={question.id}>{question.prompt}</label>
             {question.description}
             <textarea ref={inputRef} id={question.id} data-question-id={question.id}
-            value={response} onChange={(e) => {setResponse(e.target.value)}}/>
+                value={response} onChange={(e) => { setResponse(e.target.value) }} />
         </div>
     );
 }
